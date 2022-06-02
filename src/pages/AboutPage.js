acb2b6a76eb89834b8072e61e63abe7470656877
benassiPb <benassipb@gmail.com>
2022-06-01 19:45:52 -0700

    setup, nav

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import styled from 'styled-components'
+import { PageHero } from '../components'
+import aboutImg from '../assets/hero-bcg.jpeg'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="desk" />
+        <article>
+          <div className='title'>
+            <h2>Our Story</h2>
+            <div className='underline'></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum tempora magnam eius veritatis hic sit tenetur dolorem sunt rerum facilis, iure asperiores nemo cumque earum impedit molestias fugit porro! Inventore, quaerat sed. Assumenda, labore, sit iure laudantium corrupti illum, tenetur quasi cupiditate omnis sed repellat. Magnam eum pariatur commodi facilis.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  )
+}
+
+const Wrapper = styled.section`
+  display: grid;
+  gap: 4rem;
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    height: 500px;
+    object-fit: cover;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+    color: var(--clr-grey-5);
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`
+export default AboutPage
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import About from './AboutPage'
+import Home from './HomePage'
+import Cart from './CartPage'
+import Checkout from './CheckoutPage'
+import Error from './ErrorPage'
+import PrivateRoute from './PrivateRoute'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+
+
+
+export { About, Home, Cart, Checkout, Error, PrivateRoute, Products, SingleProduct }
