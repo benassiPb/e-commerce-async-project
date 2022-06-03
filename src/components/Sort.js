d72b88bce99072c8bbf4abbbc6115c8aae354098
benassiPb <benassipb@gmail.com>
2022-06-02 18:56:05 -0700

    filtering in progress

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -2,8 +2,20 @@ import React from 'react'
 import { useFilterContext } from '../context/filter_context'
 import { BsFillGridFill, BsList } from 'react-icons/bs'
 import styled from 'styled-components'
-const Sort = () => {
-  return <h4>sort </h4>
+const Sort = ({ }) => {
+  const { filtered_products: products, grid_view } = useFilterContext()
+
+  return <Wrapper>
+    <div className='btn-container'>
+      <button type='button' className={`${grid_view ? 'active' : null}`}>
+        <BsFillGridFill />
+      </button>
+      <button type='button' className={`${!grid_view ? 'active' : null}`}>
+        <BsList />
+      </button>
+    </div>
+    <p>{products.length} products found </p>
+  </Wrapper>
 }
 
 const Wrapper = styled.section`
